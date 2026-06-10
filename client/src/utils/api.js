02ea6dc15d8d8ac89084e4db77eabda61c84c307
Amit Kumar Singh <amitsingh.ghz27@gmail.com>
2026-06-10 15:59:22 +0530

    Add api url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,18 +1,32 @@
 import axios from 'axios';
 
+const API_URL =
+  process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: API_URL,
   timeout: 30000,
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 api.interceptors.response.use(
-  r => r,
-  err => {
-    if (err.response?.status === 401) {
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
       localStorage.removeItem('token');
       window.location.href = '/login';
     }
-    return Promise.reject(err);
+
+    return Promise.reject(error);
   }
 );
 
